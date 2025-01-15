cd538bc637ca77d8103ef271c4788403bd7a0949
serraguti <pacoserranox@gmail.com>
2025-01-15 17:50:25 +0100

    sdadsad

--- src/EjecucionArrays.java
+++ src/EjecucionArrays.java
@@ -1,11 +1,8 @@
 public class EjecucionArrays {
     public static void main(String[] args) {
-        //NECESITAMOS UN ARRAY DE 3 POSICIONES
-        //CON TIPO int
-        int[] numeros = new int[15];
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int) (Math.random() * 100);
-        }
+        //RECUPERAMOS UN ARRAY CON EL TAMAÑO QUE DESEEMOS
+        int[] numeros = JuegoArrays.getArrayRandom(3);
+
         int suma = JuegoArrays.sumarNumerosArray(numeros);
         System.out.println("La suma es: " + suma);
     }
--- src/JuegoArrays.java
+++ src/JuegoArrays.java
@@ -1,4 +1,14 @@
 public class JuegoArrays {
+
+    public static int[] getArrayRandom(int elementos){
+        int[] numeros = new int[elementos];
+        for (int i = 0; i < numeros.length; i++){
+            numeros[i] = (int) (Math.random() * 100);
+            System.out.println("Random: " + numeros[i]);
+        }
+        return numeros;        
+    }
+
     public static int sumarNumerosArray(int[] numeros){
         int suma = 0;
         for (int i = 0; i < numeros.length; i++){
