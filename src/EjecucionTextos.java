cd306bfc660943e76c3802e43e2972770f337ef1
serraguti <pacoserranox@gmail.com>
2025-01-15 20:36:18 +0100

    miercolesfinal

--- src/EjecucionTextos.java
+++ src/EjecucionTextos.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class EjecucionTextos {
     public static void main(String[] args) {
+        String isbn = "8441513929";
+        boolean respuesta = JuegoTextos.validarISBN(isbn);
+        if (respuesta == true){
+            System.out.println("ISBN correcto");
+        }else{
+            System.out.println("El numero ISBN es incorrecto");
+        }
+
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduzca un texto numérico");
         String texto = teclado.nextLine();
--- src/JuegoTextos.java
+++ src/JuegoTextos.java
@@ -1,13 +1,32 @@
 public class JuegoTextos {
-   public static int getSumarTexto(String texto){
+
+    public static boolean validarISBN(String isbn){
+        int sumaisbn = 0;
+        //RECORREMOS CADA CARACTER DEL TEXTO
+        for (int i = 0; i < isbn.length(); i++){
+            char letra = isbn.charAt(i);
+            String temporal = String.valueOf(letra);
+            int numero = Integer.parseInt(temporal);
+            int posicion = i + 1;
+            int operacion = numero * posicion;
+            sumaisbn += operacion;
+        }
+        if (sumaisbn%11==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static int getSumarTexto(String texto) {
         int suma = 0;
-        for (int i = 0; i < texto.length(); i++){
+        for (int i = 0; i < texto.length(); i++) {
             char letra = texto.charAt(i);
             String temporal = String.valueOf(letra);
-            //SABEMOS CONVERTIR UN String A int?
+            // SABEMOS CONVERTIR UN String A int?
             int numero = Integer.parseInt(temporal);
             suma += numero;
-        }        
+        }
         return suma;
-   }
+    }
 }
