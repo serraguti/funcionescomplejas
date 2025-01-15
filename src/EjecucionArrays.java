52e7775c554b253aec2481d3ef34dc56ea15a84b
serraguti <pacoserranox@gmail.com>
2025-01-15 17:31:14 +0100

    dfsfdfs

--- src/EjecucionArrays.java
+++ src/EjecucionArrays.java
@@ -2,10 +2,10 @@ public class EjecucionArrays {
     public static void main(String[] args) {
         //NECESITAMOS UN ARRAY DE 3 POSICIONES
         //CON TIPO int
-        int[] numeros = new int[3];
-        numeros[0] = 99;
-        numeros[1] = 2;
-        numeros[2] = 1;
+        int[] numeros = new int[15];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random() * 100);
+        }
         int suma = JuegoArrays.sumarNumerosArray(numeros);
         System.out.println("La suma es: " + suma);
     }
